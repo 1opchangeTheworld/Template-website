f3983811016355c5c14648b7cc7e98d5b13ac007
topreal <1opchagetheworld@gmail.com>
2024-05-28 22:03:22 +0700

    Add project files.

---
+++ NYK.WebApp/Areas/Main/Controllers/AuthenController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NYK.WebApp.Areas.Main.Controllers
+{
+    [Area("Main")]
+    public class AuthenController : Controller
+    {
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
---
+++ NYK.WebApp/wwwroot/assets/js/site.js
@@ -0,0 +1,7 @@
+﻿
+
+const hamBurger = document.querySelector(".toggle-btn");
+
+hamBurger.addEventListener("click", function () {
+    document.querySelector("#sidebar").classList.toggle("expand");
+});
